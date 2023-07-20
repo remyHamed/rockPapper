aeaa91902b1b273d0b6f8eec1ed96b6d6ddbd01d
hemarydem <hamedremy@hotmail.fr>
2023-07-20 22:33:21 +0200

    feature: reading stdin json

--- test.js
+++ test.js
@@ -1,30 +1,24 @@
 process.stdin.resume();
-
 process.stdin.setEncoding('utf8');
 
 let json = '';
 
 process.stdin.on('data', function(chunk) {
-    data += chunk;
+    json += chunk;
 
-    // Check if data ends with two newline characters
-    while (data.endsWith('\n\n')) {
-        // Remove trailing newlines
+    while (json.endsWith('\n')) {
         json = json.slice(0, -2);
 
         try {
             const jsonData = JSON.parse(json);
             console.log('Received JSON data:', jsonData);
-            // Now you can work with jsonData as a JavaScript object
         } catch (error) {
             console.error('Error parsing JSON:', error);
         }
 
-        // Clear data for next JSON object
-        data = '';
+        json = '';
     }
 });
-
 
 process.on('SIGINT', function() {
     process.exit(0);
